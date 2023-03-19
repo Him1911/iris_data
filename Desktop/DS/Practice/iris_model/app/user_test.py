bcc05044d113122a71e3059dd0e9b55478a77e1a
Himanshu <himanshugurav95@gmail.com>
2023-03-19 16:42:31 +0530

    running model added completely

--- Desktop/DS/Practice/iris_model/app/user_test.py
+++ Desktop/DS/Practice/iris_model/app/user_test.py
@@ -2,44 +2,49 @@ import pickle
 import json
 import numpy as np
 import os 
-
-
+from app import config_path
+# from flask import request
 
 class Prediction():
     def __init__(self):
         print(os.getcwd())
 
+    def load_raw(self):
+        with open(config_path.MODEL_PATH,'rb') as model_file:
+            self.logi_model = pickle.load(model_file)
 
-    with open(config_path.MODEL_PATH,'rb') as file:
-        model = pickle.load(file)
+        with open(config_path.ASSET_PATH,'r') as col_file:
+            self.asset = json.load(col_file)
 
-    with open(config_path.ASSET_PATH,'r') as file:
-        asset = json.load(file)
+        # col = self.asset['columns']c
 
-    col = asset['columns']
+        print(f"we are in load raw")
 
-    def data():
-        input_data = request.form
-        print(input_data)
+    def iris_pred(self,data):
+        self.load_raw()
+        self.data= data
+
         
-        data = np.zeros(len(col))
-        data[0] = input_data['sepal_length']
-        data[1] = input_data['sepal_width']
-        data[2] = input_data['petal_length']
-        data[3] = input_data['petal_width']
-        
-        result = model.predict([data])
+        data = np.zeros(len(self.asset['columns']))
+        data[0] = self.data['sepal_length']
+        data[1] = self.data['sepal_width']
+        data[2] = self.data['petal_length']
+        data[3] = self.data['petal_width']
+
+        result = self.logi_model.predict([data])
         print(result)
 
+
         if result[0] == 1:
-            iris_value = "SETOSA"
+            iris_predicted = "SETOSA"
         if result[0] == 2:
-            iris_value = "VERSICOLOR"
+            iris_predicted = "VERSICOLOR"
         if result[0] == 3:
-            iris_value = "VIRGINICA"
-            return result
+            iris_predicted = "VIRGINICA"
     
-# if __name__ == "__main__":
+        return iris_predicted
+    
+if __name__ == "__main__":
  
-#     pred_obj = Prediction()
-#     pred_obj.load_raw()
+    pred_obj= Prediction()
+    pred_obj.load_raw()
--- Desktop/DS/Practice/iris_model/main.py
+++ Desktop/DS/Practice/iris_model/main.py
@@ -1,44 +1,20 @@
 from flask import Flask, render_template, request
 import config_path
-import app.user_test
+from app.user_test import Prediction
 app=Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("front_end.html")
 
-@app.route("/get_data", methods = ["POST"])
-def data():
-    input_data = request.form
-    print(input_data)
-    return render_template("front_end.html",PREDICT_VALUE=iris_value)
+@app.route("/get_data", methods = ["POST","GET"])
+def iris_pred():
+    data = request.form
+    pred_obj= Prediction()
+    iris_predicted = pred_obj.iris_pred(data)
+    print(iris_predicted)
+    return render_template("front_end.html", PREDICT_VALUE= iris_predicted)
 
 if __name__ == '__main__':
     app.run(host=config_path.HOST_NAME, port= config_path.PORT_NUMBER, debug =True)
 
-
-
-
-
-
-# 00000000000000000000000000000000
-
-
-    
-    # data = np.zeros(len(col))
-    # data[0] = input_data['sepal_length']
-    # data[1] = input_data['sepal_width']
-    # data[2] = input_data['petal_length']
-    # data[3] = input_data['petal_width']
-    
-    # result = model.predict([data])
-    # print(result)
-
-    # if result[0] == 1:
-    #     iris_value = "SETOSA"
-    # if result[0] == 2:
-    #     iris_value = "VERSICOLOR"
-    # if result[0] == 3:
-    #     iris_value = "VIRGINICA"
-
-    # 
